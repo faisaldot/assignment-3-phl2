3ac1fd64f85ab9ef0d34c7581d2d15fecdfec073
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-06-20 19:54:51 +0600

    Added timestamps to bookSchema

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -16,25 +16,30 @@ export interface IBook extends Document {
   available: boolean;
 }
 
-const bookSchema = new Schema<IBook>({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: {
-    type: String,
-    enum: [
-      "FICTION",
-      "NON_FICTION",
-      "SCIENCE",
-      "HISTORY",
-      "BIOGRAPHY",
-      "FANTASY",
-    ],
-    required: true,
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      required: true,
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: { type: String },
+    copies: { type: Number, required: true, min: 0 },
+    available: { type: Boolean, default: true },
   },
-  isbn: { type: String, required: true, unique: true },
-  description: { type: String },
-  copies: { type: Number, required: true, min: 0 },
-  available: { type: Boolean, default: true },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Book = model<IBook>("Book", bookSchema);
