21c4db492ac14c116b8d97155632d192b6745004
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-06-21 15:05:22 +0600

    Completed 7. Borrowed Books Summary (Using Aggregation)

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -59,4 +59,40 @@ borrowRouter.post("/", async (req, res) => {
   }
 });
 
+borrowRouter.get("/", async (req, res) => {
+  try {
+    const borrowBook = await Borrow.aggregate([
+      { $group: { _id: "$book", totalQuantity: { $sum: "$quantity" } } },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookDetails",
+        },
+      },
+      { $unwind: "$bookDetails" },
+      {
+        $project: {
+          _id: 0,
+          book: { title: "$bookDetails.title", isbn: "$bookDetails.isbn" },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: borrowBook,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: "Failed to retrieve borrowed books summary",
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
 export default borrowRouter;
